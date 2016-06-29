b17868051e9c4719e8c294d3b81febdd7e66b487
Eric Adams <eric@clearviewsocial.com>
2016-06-29 17:29:28 +0000

    Update to latest pm2 api conventions

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-
 process.env.MODULE_DEBUG = (process.NODE_ENV == 'production' ? false : true);
 
-var pmx     = require('pmx');
-var pm2     = require('pm2');
+var pmx = require('pmx');
 
-var SysLogger = require('ain2');
-var logger    = new SysLogger({facility: 'syslog'});
+var conf = pmx.initModule({}, function (err, conf) {
 
-var conf    = pmx.initModule();
+    var SysLogger = require('ain2');
+    var logger = new SysLogger();
+    var pm2 = require('pm2');
 
+    pm2.launchBus(function (err, bus) {
+        bus.on('*', function (event, msg) {
+            if (event == 'process:event' && msg.event == 'online') {
+                logger.log('Process %s restarted %s', msg.process.name, msg.process.restart_time, function () {
+                    console.log(arguments)
+                });
+                console.log('Process %s restarted %s', msg.process.name, msg.process.restart_time);
+            }
+            if (event == 'log:err') {
+                logger.error('%s', msg.data);
+                console.log(msg.data);
+            }
+        });
+    });
 
-pm2.launchBus(function(err, bus) {
-  bus.on('*', function(event, data){
-    if (event == 'process:event' && data.event == 'online') {
-      logger.log('Process %s restarted %s', data.process.name, data.process.restart_time, function() { console.log(arguments)});
-      console.log('Process %s restarted %s', data.process.name, data.process.restart_time);
-    }
-  });
 });
